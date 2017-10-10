af2a7c36eb2f418cc68144c1314898e35c7e3c93
INFINITYACE-PC\InfinityAce <zechariah.tan.jl@gmail.com>
2017-10-11 07:00:43 +0800

    Add NOK Name & Phone to SampleDataUtil.java. Add NOK Name & Phone to hashCode method in Person class.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -165,7 +165,7 @@ public class Person implements ReadOnlyPerson {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, nokName, nokPhone, tags);
     }
 
     @Override
