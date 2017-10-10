9fc058fa7f11df7004a077226cacc6de9f07e495
Xie Yu Heng <xieyuheng222@hotmail.com>
2017-10-10 20:57:05 +0800

    Add sort method to ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -133,6 +133,11 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
             fail("This method should not be called.");
         }
+
+        @Override
+        public void sort() {
+            fail("This method should not be called.");
+        }
     }
 
     /**
