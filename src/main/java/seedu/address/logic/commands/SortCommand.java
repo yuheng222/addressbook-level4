47f3f3697b59983f0c2efeaa9839eb56b77727b8
Xie Yu Heng <xieyuheng222@hotmail.com>
2017-10-10 20:02:29 +0800

    Implement SortCommand Class

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+/**
+ * Sorts all persons in the address book lexicographically.
+ */
+
+public class SortCommand extends UndoableCommand{
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_SUCCESS = "Sorted all persons in the Address Book";
+
+    @Override
+    public CommandResult executeUndoableCommand() {
+        model.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
