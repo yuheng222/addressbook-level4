a72b0a0f8c11aa09c3d65c33873c8151f9a60a1e
Wang Jie <onlittlejieee@200-217.priv23.nus.edu.sg>
2017-10-10 21:38:04 -0700

    add Junit test for filter command

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -15,22 +15,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.HistoryCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonHasTagPredicate;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
@@ -74,6 +64,14 @@ public class AddressBookParserTest {
     public void parseCommand_exit() throws Exception {
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+    }
+    
+    @Test
+    public void parseCommand_filter() throws Exception {
+        String tagKeyword = new String("tag");
+        FilterCommand command = (FilterCommand) parser.parseCommand(
+                FilterCommand.COMMAND_WORD + " " + tagKeyword);
+        assertEquals(new FilterCommand(new PersonHasTagPredicate(tagKeyword)), command);
     }
 
     @Test
---
+++ src/test/java/seedu/address/model/person/PersonHasTagPredicateTest.java
@@ -0,0 +1,64 @@
+package seedu.address.model.person;
+
+import org.junit.Test;
+import seedu.address.testutil.PersonBuilder;
+
+import static org.junit.Assert.*;
+
+public class PersonHasTagPredicateTest {
+    @Test
+    public void test_personHasTag_returns_true(){
+        //person has only one tag
+        PersonHasTagPredicate predicate = new PersonHasTagPredicate("friends");
+        assertTrue(predicate.test(new PersonBuilder().build()));
+        
+        //person has multiple tags
+        predicate = new PersonHasTagPredicate("friends");
+        assertTrue(predicate.test(new PersonBuilder().withTags("friends", "colleagues").build()));
+        
+        //different case
+        predicate = new PersonHasTagPredicate("FRIENDS");
+        assertTrue(predicate.test(new PersonBuilder().withTags("fRiEnds").build()));
+    }
+    
+    @Test
+    public void test_personHasTag_returns_false(){
+        //no keyword
+        PersonHasTagPredicate predicate = new PersonHasTagPredicate("");
+        assertFalse(predicate.test(new PersonBuilder().build()));
+        
+        //keyword does not match
+        predicate = new PersonHasTagPredicate("families");
+        assertFalse(predicate.test(new PersonBuilder().withTags("friends", "colleagues").build()));
+
+        //keyword matches name, but not tag name
+        predicate = new PersonHasTagPredicate("Alice");
+        assertFalse(predicate.test(new PersonBuilder().withName("Alice").build()));
+    } 
+        
+    @Test
+    public void equals() throws Exception {
+        String firstPredicateKeyword = new String("first");
+        String secondPredicateKeyword = new String("second");
+
+        PersonHasTagPredicate firstPredicate = new PersonHasTagPredicate(firstPredicateKeyword);
+        PersonHasTagPredicate secondPredicate = new PersonHasTagPredicate(secondPredicateKeyword);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        PersonHasTagPredicate firstPredicateCopy = new PersonHasTagPredicate(firstPredicateKeyword);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different tags -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+}
