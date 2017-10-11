7d4fc58b1d7808f23431ee7f4e8cffbc3ffa5d3e
INFINITYACE-PC\InfinityAce <zechariah.tan.jl@gmail.com>
2017-10-11 23:40:44 +0800

    Fix to style in NOK Name & Phone files by shifting static import statements up

--- src/main/java/seedu/address/model/person/NokName.java
+++ src/main/java/seedu/address/model/person/NokName.java
@@ -1,8 +1,8 @@
 package seedu.address.model.person;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-
 import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a Person's Next-of-Kin's name in the address book.
