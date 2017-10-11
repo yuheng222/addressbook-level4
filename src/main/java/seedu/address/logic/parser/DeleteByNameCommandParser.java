aa2053a4c3a0c4a689007fb049dcd7920ace5359
Teo Meng Shin, Ryan <wanganpro@gmail.com>
2017-10-11 22:10:59 +0800

    Fix comments in DeleteByNameCommandParser.

--- src/main/java/seedu/address/logic/parser/DeleteByNameCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteByNameCommandParser.java
@@ -8,13 +8,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Name;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteByNameCommand object
  */
 public class DeleteByNameCommandParser implements Parser<DeleteByNameCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns an DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteByNameCommand
+     * and returns a DeleteByNameCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteByNameCommand parse(String args) throws ParseException {
