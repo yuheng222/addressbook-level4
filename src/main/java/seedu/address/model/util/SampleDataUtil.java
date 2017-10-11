f94ad9b275af44a80f3f4abae7fa779168b231dc
INFINITYACE-PC\InfinityAce <zechariah.tan.jl@gmail.com>
2017-10-11 23:33:34 +0800

    Fix to SampleDataUtil.java by importing NOK Name & Phone files

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -9,6 +9,8 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.NokName;
+import seedu.address.model.person.NokPhone;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
