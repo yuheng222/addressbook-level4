38816fe6635357a9aac782911b3ccb2b28a39fc3
Xie Yu Heng <xieyuheng222@hotmail.com>
2017-10-11 17:24:08 +0800

    Fix minor checkstyle error due to extra line

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
