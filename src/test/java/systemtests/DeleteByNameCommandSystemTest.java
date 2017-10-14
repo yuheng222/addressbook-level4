00ff451f9c4c79110efb29d88af5b273f237d7d1
Teo Meng Shin, Ryan <wanganpro@gmail.com>
2017-10-14 23:13:46 +0800

    Fix a typo in DeleteByNameCommandSystemTest

--- src/test/java/systemtests/DeleteByNameCommandSystemTest.java
+++ src/test/java/systemtests/DeleteByNameCommandSystemTest.java
@@ -193,7 +193,7 @@ public class DeleteByNameCommandSystemTest extends AddressBookSystemTest {
     /**
      * Performs the same verification as {@code assertCommandSuccess(String, Model, String)} except that the browser url
      * and selected card are expected to update accordingly depending on the card at {@code expectedSelectedCardIndex}.
-     * @see DeleteCommandSystemTest#assertCommandSuccess(String, Model, String)
+     * @see DeleteByNameCommandSystemTest#assertCommandSuccess(String, Model, String)
      * @see AddressBookSystemTest#assertSelectedCardChanged(Index)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage,
