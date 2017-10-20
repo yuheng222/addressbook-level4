666bf165cc5a960abaf5b561fb1f8dd054bbfafa
Teo Meng Shin, Ryan <wanganpro@gmail.com>
2017-10-20 21:14:32 +0800

    Fix PREFIX_NOK_PHONE causing AddCommandParserTest to fail.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -62,7 +62,7 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_NOK_NAME_DESC = " " + PREFIX_NOK_NAME + "Jessie%"; // '%' not allowed in names
-    public static final String INVALID_NOK_PHONE_DESC = " " + PREFIX_PHONE + "999m"; // 'm' not allowed in phones
+    public static final String INVALID_NOK_PHONE_DESC = " " + PREFIX_NOK_PHONE + "999m"; // 'm' not allowed in phones
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
