9b89c905f6deba0410f4e63afca29017e6ffb77d
Wang Jie <fjfqwangj163@gmail.com>
2017-10-20 07:19:00 -0700

    Update AddTagCommand.java

--- src/main/java/seedu/address/logic/commands/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -14,6 +14,8 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.NokName;
+import seedu.address.model.person.NokPhone;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
