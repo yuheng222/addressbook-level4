d161d6a40e0da9a4e1bbc82087e59348bc358e18
Xie Yu Heng <xieyuheng222@hotmail.com>
2017-10-22 20:46:42 +0800

    Add select theme command parser test

---
+++ src/test/java/seedu/address/logic/parser/SelectThemeCommandParserTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_THEME;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SelectThemeCommand;
+
+/**
+ * Test scope: similar to {@code DeleteCommandParserTest}.
+ * @see DeleteCommandParserTest
+ */
+public class SelectThemeCommandParserTest {
+
+    private SelectThemeCommandParser parser = new SelectThemeCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSelectCommand() {
+        assertParseSuccess(parser, "1", new SelectThemeCommand(INDEX_FIRST_THEME));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectThemeCommand.MESSAGE_USAGE));
+    }
+}
