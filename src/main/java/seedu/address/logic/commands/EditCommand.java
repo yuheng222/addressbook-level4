08e80d73b14f2f0b5a0f14056cfef26308c3b23e
Xavier Xie <xieyuheng222@hotmail.com>
2017-10-28 00:06:47 +0800

    Fix syntax error

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -205,10 +205,12 @@ public class EditCommand extends UndoableCommand {
 
         public void setAvatar(Avatar avatar) {
             this.avatar = avatar;
-
+        }
+        
         public Optional<Avatar> getAvatar() {
             return Optional.ofNullable(avatar);
-
+        }
+        
         public void setNokName(NokName nokName) {
             this.nokName = nokName;
         }
