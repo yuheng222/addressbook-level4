5ce9ab1dc74df3c61544fbf6c4daa930cd3f2d2a
Teo Meng Shin, Ryan <wanganpro@gmail.com>
2017-10-29 03:07:11 +0800

    Add basic File IO to ExportCommand.

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands;
 
-import java.lang.StringBuilder;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
-
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -10,14 +11,17 @@ import seedu.address.model.person.ReadOnlyPerson;
 /**
  * Exports the current Address Book data into a CSV file.
  */
-
 public class ExportCommand extends Command {
 
     public static final String MESSAGE_EMPTY_ADDRESS_BOOK = "Export failed. Current Address Book is empty.";
 
+    public static final String DATA_FILE_PATH = "AddressBookData.csv";
+
+    //public static final String MESSAGE_IO_EXCEPTION = "Read/Write Exception Occurred";
+
     private final List<ReadOnlyPerson> currentData;
 
-    public ExportCommand() {this.currentData = model.getAddressBook().getPersonList();}
+    public ExportCommand() { this.currentData = model.getAddressBook().getPersonList(); }
 
     @Override
     public CommandResult execute() throws CommandException {
@@ -26,9 +30,18 @@ public class ExportCommand extends Command {
             throw new CommandException(MESSAGE_EMPTY_ADDRESS_BOOK);
         }
 
+        if (fileExists()) {
+            deleteFile();
+        }
+
+        createFile();
+
+        writeData();
+
         return null;
     }
 
+    /** Constructing the headers for CSV */
     private String constructHeaders() {
         ReadOnlyPerson samplePerson = currentData.get(0);
         List<String> propertyNames = samplePerson.getPropertyNamesAsList();
@@ -44,6 +57,39 @@ public class ExportCommand extends Command {
         builder.setCharAt(lastChar, '\n');
 
         return builder.toString();
+    }
+
+    /** Function to write data to the CSV */
+    private void writeData() throws CommandException {
+        File csvData = new File(DATA_FILE_PATH);
+        String headers = constructHeaders();
+        try {
+            PrintWriter pw = new PrintWriter(csvData);
+            pw.write(headers);
+        } catch (IOException ioe) {
+            throw new CommandException(ioe.getMessage());
+        }
+
+    }
+
+    /** Function to create the CSV */
+    private void createFile() throws CommandException {
+        try {
+            File csvData = new File(DATA_FILE_PATH);
+            csvData.createNewFile();
+        } catch (IOException ioe) {
+            throw new CommandException(ioe.getMessage());
+        }
+    }
+
+    private boolean fileExists() {
+        File csvData = new File(DATA_FILE_PATH);
+        return csvData.exists();
+    }
+
+    private void deleteFile() {
+        File csvData = new File(DATA_FILE_PATH);
+        csvData.delete();
     }
 
 }
