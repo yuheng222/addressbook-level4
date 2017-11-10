915bd35f6590c38ef3a91e72123b78cb0b16930f
INFINITYACE-PC\InfinityAce <zechariah.tan.jl@gmail.com>
2017-11-10 22:30:41 +0800

    Add collated code for myself, Update to MainApp to reflect address book name and version number

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -40,7 +40,7 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 3, 0, true);
+    public static final Version VERSION = new Version(1, 5, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
@@ -55,7 +55,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("============================[ Initializing Address Book & Booklets ]==========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
